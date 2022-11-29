f30b2c37ee99f99a9fe64cef51effbed024fc22b
Sara Mohamed <97265093+saraamohameed@users.noreply.github.com>
2022-11-29 06:45:44 +0200

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,17 @@
+
+num_1 = float(input("enter the first num:"))
+operator = input("enter the oprator:")
+num_2 = float(input("enter the second num:"))
+
+if operator == "+":
+	print(num_1+num_2)
+
+if operator  == "-":
+	print(num_1  - num_2)
+
+elif operator  == "*":
+	print(num_1 * num_2)
+
+elif operator  == "/":
+	print(num_1 / num_2)
+	
